9fbe9a8ea59f753dfb6781ad69eedc3dd6c2dcce
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2018-07-13 00:15:57 +0100

    Interrupt subsystem refactoring

---
+++ src/arch/x86_64/int/apic.rs
@@ -0,0 +1,26 @@
+use ::arch::int::InterruptController;
+use ::arch::dev::ioapic::IOAPIC;
+use ::arch::dev::lapic::LAPIC;
+use ::arch::acpi::ACPI;
+
+pub struct Controller {}
+
+pub static CONTROLLER: Controller = Controller {};
+
+impl InterruptController for Controller {
+    fn end_of_int(&self) {
+        LAPIC.irq().end_of_int();
+    }
+
+    fn irq_remap(&self, irq: u32) -> u32 {
+        ACPI.lock().get_irq_mapping(irq)
+    }
+
+    fn mask_int(&self, int: u8, masked: bool) {
+        IOAPIC.lock().mask_int(self.irq_remap(int as u32), masked);
+    }
+
+    fn set_irq_dest(&self, src: u8, dst: u8) {
+        IOAPIC.lock().set_int(self.irq_remap(src as u32), dst as u32);
+    }
+}

633d8324aa2712e4da8bc2a3d124e2827be4cacf
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2019-03-30 18:17:38 +0000

    Major scheduler refactoring

--- src/arch/x86_64/mm/heap.rs
+++ src/arch/x86_64/mm/heap.rs
@@ -2,5 +2,5 @@ use kernel::mm::VirtAddr;
 
 pub const HEAP_START: VirtAddr = VirtAddr(0xfffff80000000000);
 pub const HEAP_SIZE: usize = 1 * 4096; // 4KB / 1 pages // heap will grow when more memory is needed
-pub const HEAP_END: VirtAddr = VirtAddr(HEAP_START.0 + (4096 * 4096) as usize); // 4MB
+pub const HEAP_END: VirtAddr = VirtAddr(HEAP_START.0 + (4096*4096) as usize); // 4MB
 
---
+++ src/kernel/sched/cpu_queues.rs
@@ -0,0 +1,85 @@
+use alloc::sync::Arc;
+use core::cell::UnsafeCell;
+
+use kernel::sync::Mutex;
+use kernel::task::Task;
+use kernel::utils::PerCpu;
+
+use super::CpuQueue;
+
+pub struct CpuQueues {
+    cpu_queues_locks: PerCpu<Mutex<()>>,
+    cpu_queues: PerCpu<UnsafeCell<CpuQueue>>,
+}
+
+unsafe impl Sync for CpuQueues{}
+
+impl Default for CpuQueues {
+    fn default() -> CpuQueues {
+        CpuQueues {
+            cpu_queues_locks: PerCpu::new_fn(|| {
+                Mutex::<()>::new(())
+            }),
+            cpu_queues: PerCpu::new_fn(|| {
+                UnsafeCell::new(CpuQueue::default())
+            })
+        }
+    }
+}
+
+impl CpuQueues {
+
+    unsafe fn this_cpu_queue(&self) -> &mut CpuQueue {
+        (&mut *(self.cpu_queues.this_cpu_mut().get()))
+    }
+
+    pub fn schedule_next(&self) {
+        let mutex = self.cpu_queues_locks.this_cpu().lock_irq();
+
+        unsafe {
+            self.this_cpu_queue().schedule_next(mutex);
+        }
+    }
+
+    pub fn reschedule(&self) -> bool {
+        let mutex = self.cpu_queues_locks.this_cpu().lock_irq();
+
+        unsafe {
+            self.this_cpu_queue().reschedule(mutex)
+        }
+    }
+
+    pub fn enter_critical_section(&self) {
+        let _mutex = self.cpu_queues_locks.this_cpu().lock_irq();
+
+        unsafe {
+            self.this_cpu_queue().enter_critical_section();
+        }
+    }
+
+    pub fn leave_critical_section(&self) {
+        let mutex = self.cpu_queues_locks.this_cpu().lock_irq();
+
+        unsafe {
+            self.this_cpu_queue().leave_critical_section(mutex);
+        }
+
+    }
+
+    pub fn add_task(&self, task: Arc<Task>) {
+        let _mutex = self.cpu_queues_locks.this_cpu().lock_irq();
+
+        unsafe {
+            self.this_cpu_queue().add_task(task);
+        }
+    }
+
+    pub fn current_task_finished(&self) {
+        let mutex = self.cpu_queues_locks.this_cpu().lock_irq();
+
+        unsafe {
+            self.this_cpu_queue().current_task_finished(mutex);
+        }
+    }
+}
+

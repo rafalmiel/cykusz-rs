0bfcbdfb61efb2b51bd5cb6cf7139cdfcac0fe07
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2020-02-07 14:16:19 +0000

    userspace: open stdin/stdout in the user process

--- cykusz-rs/src/kernel/fs/stdio.rs
+++ cykusz-rs/src/kernel/fs/stdio.rs
@@ -5,8 +5,11 @@ use spin::Once;
 
 use crate::kernel::fs::inode::INode;
 use crate::kernel::fs::vfs::Result;
+use crate::kernel::device::{Device, alloc_id, register_device};
 
-pub struct StdOut {}
+pub struct StdOut {
+    id: usize,
+}
 
 impl INode for StdOut {
     fn write_at(&self, _offset: usize, buf: &[u8]) -> Result<usize> {
@@ -15,7 +18,9 @@ impl INode for StdOut {
     }
 }
 
-pub struct StdIn {}
+pub struct StdIn {
+    id: usize,
+}
 
 impl INode for StdIn {
     fn read_at(&self, _offset: usize, buf: &mut [u8]) -> Result<usize> {
@@ -29,6 +34,34 @@ impl INode for StdIn {
 static STDOUT: Once<Arc<StdOut>> = Once::new();
 static STDIN: Once<Arc<StdIn>> = Once::new();
 
+impl Device for StdIn {
+    fn id(&self) -> usize {
+        self.id
+    }
+
+    fn name(&self) -> String {
+        String::from("stdin")
+    }
+
+    fn inode(&self) -> Arc<dyn INode> {
+        stdin().clone()
+    }
+}
+
+impl Device for StdOut {
+    fn id(&self) -> usize {
+        self.id
+    }
+
+    fn name(&self) -> String {
+        String::from("stdout")
+    }
+
+    fn inode(&self) -> Arc<dyn INode> {
+        stdout().clone()
+    }
+}
+
 pub fn stdout() -> &'static Arc<StdOut> {
     &STDOUT.r#try().unwrap()
 }
@@ -38,6 +71,13 @@ pub fn stdin() -> &'static Arc<StdIn> {
 }
 
 pub fn init() {
-    STDOUT.call_once(|| Arc::new(StdOut {}));
-    STDIN.call_once(|| Arc::new(StdIn {}));
+    STDOUT.call_once(|| Arc::new(StdOut {
+        id: alloc_id()
+    }));
+    STDIN.call_once(|| Arc::new(StdIn {
+        id: alloc_id()
+    }));
+
+    register_device(stdin().clone()).expect("Failed to register stdin device");
+    register_device(stdout().clone()).expect("Failed to register stdout device");
 }

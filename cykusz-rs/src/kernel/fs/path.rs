7d67ee98106a167a30a9a4f4208da12ebc60b941
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2020-02-10 23:27:42 +0000

    syscall: mkdir refactoring

--- cykusz-rs/src/kernel/fs/path.rs
+++ cykusz-rs/src/kernel/fs/path.rs
@@ -1,8 +1,8 @@
 pub struct Path<'a>(&'a str);
 
 impl<'a> Path<'a> {
-    pub const fn new(path: &'a str) -> Self {
-        Path::<'a>(path)
+    pub fn new(path: &'a str) -> Self {
+        Path::<'a>(path.trim().trim_end_matches("/"))
     }
 
     pub fn is_absolute(&self) -> bool {
@@ -10,9 +10,20 @@ impl<'a> Path<'a> {
     }
 
     pub fn components(&self) -> impl Iterator<Item = &str> {
+        self.0.split("/").filter(|e| *e != "" && *e != ".")
+    }
+
+    pub fn containing_dir(&self) -> (Path<'a>, Path<'a>) {
+        let containing_dir = self.0.rfind("/");
+
+        match containing_dir {
+            Some(0) => (Path::new("/"), Path::new(&self.0[1..])),
+            Some(v) => (Path::new(&self.0[..v]), Path::new(&self.0[v + 1..])),
+            None => (Path::new(""), Path::new(self.0)),
+        }
+    }
+
+    pub fn str(&self) -> &'a str {
         self.0
-            .trim_start_matches("/")
-            .split("/")
-            .filter(|e| *e != "" && *e != ".")
     }
 }

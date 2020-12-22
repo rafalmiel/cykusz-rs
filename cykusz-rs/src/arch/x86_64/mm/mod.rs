7ebbf16b11974bea6a2b57519e00095500139c93
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2020-12-22 19:12:49 +0100

    ahci: regs wip

--- cykusz-rs/src/arch/x86_64/mm/mod.rs
+++ cykusz-rs/src/arch/x86_64/mm/mod.rs
@@ -6,21 +6,21 @@ pub mod phys;
 pub mod virt;
 
 const VIRT: VirtAddr = VirtAddr(0xFFFFFF0000000000);
-const PHYSMAP: MappedAddr = MappedAddr(0xFFFF800000000000);
+const MAPPED: MappedAddr = MappedAddr(0xFFFF800000000000);
 
 pub const PAGE_SIZE: usize = 4096;
 
 impl PhysAddr {
     pub fn to_mapped(&self) -> MappedAddr {
-        if self.0 < PHYSMAP.0 {
-            MappedAddr(self.0 + PHYSMAP.0)
+        if self.0 < MAPPED.0 {
+            MappedAddr(self.0 + MAPPED.0)
         } else {
             MappedAddr(self.0)
         }
     }
 
     pub fn to_virt(&self) -> VirtAddr {
-        if self.0 < PHYSMAP.0 {
+        if self.0 < MAPPED.0 {
             VirtAddr(self.0 + VIRT.0)
         } else {
             VirtAddr(self.0)
@@ -44,8 +44,8 @@ impl VirtAddr {
 
 impl MappedAddr {
     pub fn to_phys(&self) -> PhysAddr {
-        if self >= &PHYSMAP {
-            PhysAddr(self.0 - PHYSMAP.0)
+        if self >= &MAPPED {
+            PhysAddr(self.0 - MAPPED.0)
         } else {
             PhysAddr(self.0)
         }

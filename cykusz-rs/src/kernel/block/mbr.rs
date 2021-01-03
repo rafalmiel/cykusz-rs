7249301d316d499d0babb924bdaabc923d4f197d
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2021-01-03 20:53:53 +0100

    block: detect partitions from mbr sector
    Register them as block devices

---
+++ cykusz-rs/src/kernel/block/mbr.rs
@@ -0,0 +1,94 @@
+#![allow(dead_code)]
+use bit_field::BitField;
+
+use crate::arch::raw::mm::VirtAddr;
+use crate::kernel::mm::heap::{allocate_align, deallocate_align};
+
+pub struct Mbr {
+    data: VirtAddr,
+}
+
+pub struct Partition<'a> {
+    data: &'a [u8],
+}
+
+impl Mbr {
+    pub fn new() -> Mbr {
+        let mem = allocate_align(512, 0x1000).unwrap();
+
+        Mbr {
+            data: VirtAddr(mem as usize),
+        }
+    }
+
+    pub fn bytes(&self) -> &[u8] {
+        unsafe { self.data.as_bytes(512) }
+    }
+
+    pub fn bytes_mut(&mut self) -> &mut [u8] {
+        unsafe { self.data.as_bytes_mut(512) }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.bytes()[510..] == [0x55, 0xAA]
+    }
+
+    pub fn partition(&self, idx: usize) -> Option<Partition> {
+        if idx >= 4 {
+            return None;
+        }
+        let off = 0x01BEusize + 0x10 * idx;
+
+        Some(Partition {
+            data: &self.bytes()[off..off + 16],
+        })
+    }
+}
+
+impl Partition<'_> {
+    pub fn flags(&self) -> u8 {
+        self.data[0]
+    }
+
+    pub fn starting_head(&self) -> u8 {
+        self.data[1]
+    }
+
+    pub fn starting_sector(&self) -> u8 {
+        self.data[2].get_bits(0..6)
+    }
+
+    pub fn starting_cylinder(&self) -> u16 {
+        ((self.data[2].get_bits(6..8) as u16) << 8) | self.data[3] as u16
+    }
+
+    pub fn system_id(&self) -> u8 {
+        self.data[4]
+    }
+
+    pub fn ending_head(&self) -> u8 {
+        self.data[5]
+    }
+
+    pub fn ending_sector(&self) -> u8 {
+        self.data[6].get_bits(0..6)
+    }
+
+    pub fn ending_cylinder(&self) -> u16 {
+        ((self.data[6].get_bits(6..8) as u16) << 8) | self.data[7] as u16
+    }
+
+    pub fn relative_sector(&self) -> u32 {
+        unsafe { *(self.data.as_ptr().offset(8) as *const u32) }
+    }
+
+    pub fn total_sectors(&self) -> u32 {
+        unsafe { *(self.data.as_ptr().offset(12) as *const u32) }
+    }
+}
+
+impl Drop for Mbr {
+    fn drop(&mut self) {
+        deallocate_align(self.data.0 as *mut u8, 512, 0x1000);
+    }
+}
---
+++ cykusz-rs/src/kernel/block/mod.rs
@@ -0,0 +1,153 @@
+use alloc::collections::BTreeMap;
+use alloc::string::String;
+use alloc::sync::{Arc, Weak};
+use alloc::vec::Vec;
+
+use crate::kernel::device;
+use crate::kernel::device::{alloc_id, register_device, Device};
+use crate::kernel::fs::inode::INode;
+use crate::kernel::sync::RwSpin;
+
+mod mbr;
+
+pub trait BlockDev: Send + Sync {
+    fn read(&self, sector: usize, count: usize, dest: &mut [u8]) -> Option<usize>;
+    fn write(&self, sector: usize, buf: &[u8]) -> Option<usize>;
+}
+
+static BLK_DEVS: RwSpin<BTreeMap<usize, Arc<BlockDevice>>> = RwSpin::new(BTreeMap::new());
+
+pub fn register_blkdev(dev: Arc<BlockDevice>) -> device::Result<()> {
+    let mut devs = BLK_DEVS.write();
+
+    register_device(dev.clone())?;
+
+    devs.insert(dev.id, dev);
+
+    Ok(())
+}
+
+pub struct BlockDevice {
+    id: usize,
+    name: String,
+    dev: Arc<dyn BlockDev>,
+    self_ref: Weak<BlockDevice>,
+}
+
+pub struct PartitionBlockDev {
+    offset: usize, // offset in sectors
+    size: usize,   // capacity in sectors
+    dev: Arc<dyn BlockDev>,
+}
+
+impl BlockDev for PartitionBlockDev {
+    fn read(&self, sector: usize, count: usize, dest: &mut [u8]) -> Option<usize> {
+        if sector + count > self.size {
+            return None;
+        } else {
+            self.dev.read(self.offset + sector, count, dest)
+        }
+    }
+    fn write(&self, sector: usize, buf: &[u8]) -> Option<usize> {
+        let count = (buf.len() + 511) / 512;
+
+        if sector + count > self.size {
+            return None;
+        } else {
+            self.dev.write(self.offset + sector, buf)
+        }
+    }
+}
+
+impl PartitionBlockDev {
+    pub fn new(offset: usize, size: usize, dev: Arc<dyn BlockDev>) -> PartitionBlockDev {
+        PartitionBlockDev { offset, size, dev }
+    }
+}
+
+impl BlockDevice {
+    pub fn new(name: String, imp: Arc<dyn BlockDev>) -> Arc<BlockDevice> {
+        BlockDevice {
+            id: alloc_id(),
+            name,
+            dev: imp,
+            self_ref: Weak::new(),
+        }
+        .wrap()
+    }
+
+    fn wrap(self) -> Arc<BlockDevice> {
+        let fs = Arc::new(self);
+        let weak = Arc::downgrade(&fs);
+        let ptr = Arc::into_raw(fs) as *mut Self;
+        unsafe {
+            (*ptr).self_ref = weak;
+            Arc::from_raw(ptr)
+        }
+    }
+}
+
+impl INode for BlockDevice {}
+
+impl Device for BlockDevice {
+    fn id(&self) -> usize {
+        self.id
+    }
+
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn inode(&self) -> Arc<dyn INode> {
+        self.self_ref.upgrade().unwrap().clone()
+    }
+}
+
+impl BlockDev for BlockDevice {
+    fn read(&self, sector: usize, count: usize, dest: &mut [u8]) -> Option<usize> {
+        self.dev.read(sector, count, dest)
+    }
+
+    fn write(&self, sector: usize, buf: &[u8]) -> Option<usize> {
+        self.dev.write(sector, buf)
+    }
+}
+
+pub fn init() {
+    use crate::alloc::string::ToString;
+
+    let mut mbr = mbr::Mbr::new();
+
+    let mut partitions = Vec::<Arc<BlockDevice>>::new();
+
+    for (_, dev) in BLK_DEVS.read().iter() {
+        dev.read(0, 1, mbr.bytes_mut());
+
+        if mbr.is_valid() {
+            for p in 0..4 {
+                if let Some(part) = mbr.partition(p) {
+                    if part.total_sectors() > 0 {
+                        let part_dev = PartitionBlockDev::new(
+                            part.relative_sector() as usize,
+                            part.total_sectors() as usize,
+                            dev.clone(),
+                        );
+
+                        partitions.push(BlockDevice::new(
+                            dev.name() + "." + &(p + 1).to_string(),
+                            Arc::new(part_dev),
+                        ));
+                    }
+                }
+            }
+        }
+    }
+
+    for p in partitions.iter() {
+        if let Err(e) = register_blkdev(p.clone()) {
+            panic!("Failed to register blkdev {} {:?}", p.name(), e);
+        }
+
+        p.write(0, &[0xCCu8; 512]);
+    }
+}

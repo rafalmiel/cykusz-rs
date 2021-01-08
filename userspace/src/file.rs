d11a188caf456ab91a8fd9d6cc72766073e93e92
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2021-01-08 15:14:55 +0100

    ext2: Support file reading (poc)
    Add cat cmd to userspace

--- userspace/src/file.rs
+++ userspace/src/file.rs
@@ -3,18 +3,34 @@ pub struct File {
 }
 
 impl File {
-    pub fn new(path: &str, flags: syscall_defs::OpenFlags) -> File {
-        File {
-            fd: syscall::open(path, flags).expect("Failed to open file"),
+    pub fn new(path: &str, flags: syscall_defs::OpenFlags) -> Option<File> {
+        match syscall::open(path, flags) {
+            Ok(fd) => Some(File { fd }),
+            Err(e) => {
+                println!("Failed to open file: {:?}", e);
+                None
+            }
         }
     }
 
     pub fn read(&self, buf: &mut [u8]) -> usize {
-        syscall::read(self.fd, buf).expect("Failed to read a file")
+        match syscall::read(self.fd, buf) {
+            Ok(s) => s,
+            Err(e) => {
+                println!("File read failed: {:?}", e);
+                0
+            }
+        }
     }
 
     pub fn write(&self, buf: &[u8]) -> usize {
-        syscall::write(self.fd, buf).expect("Failed to write to file")
+        match syscall::write(self.fd, buf) {
+            Ok(s) => s,
+            Err(e) => {
+                println!("File write failed: {:?}", e);
+                0
+            }
+        }
     }
 }
 

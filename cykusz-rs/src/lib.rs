e601565e9041712c191acdef9e1c7283665330ee
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2021-04-30 19:50:28 +0200

    Update Cargo.lock

--- cykusz-rs/src/lib.rs
+++ cykusz-rs/src/lib.rs
@@ -7,6 +7,7 @@
 #![feature(concat_idents)]
 #![feature(const_btree_new)]
 #![feature(const_fn)]
+#![feature(const_fn_trait_bound)]
 #![feature(const_mut_refs)]
 #![feature(lang_items)]
 #![feature(linkage)]

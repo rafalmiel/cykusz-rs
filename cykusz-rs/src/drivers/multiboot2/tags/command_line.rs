8c7f421586a292da1a569f07ffc1d93d956973fc
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2023-07-01 14:08:02 +0100

    Add disks kernel options to select disks to scan

--- cykusz-rs/src/drivers/multiboot2/tags/command_line.rs
+++ cykusz-rs/src/drivers/multiboot2/tags/command_line.rs
@@ -8,12 +8,12 @@ pub struct CommandLine {
 impl CommandLine {
     pub fn command_line(&self) -> &str {
         use ::core::{mem, slice, str};
-        let strlen = self.tag.size as usize - mem::size_of::<CommandLine>();
+        let strlen = self.tag.size as usize - mem::size_of::<Tag>() - 1;
         unsafe {
-            str::from_utf8_unchecked(slice::from_raw_parts(
+            str::from_utf8(slice::from_raw_parts(
                 &self.cmdline_byte as *const u8,
                 strlen,
-            ))
+            )).unwrap()
         }
     }
 }

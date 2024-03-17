853ac9ab99cf7a663d825fbee423d9524da0b8ea
Rafal Miel <3881998+rafalmiel@users.noreply.github.com>
2024-03-17 16:08:09 +0000

    Update Cargo.lock

--- userspace/src/bin/shell/main.rs
+++ userspace/src/bin/shell/main.rs
@@ -364,7 +364,7 @@ fn exec(cmd: &str) {
         }
     } else if cmd.starts_with("date") {
         if let Ok(t) = syscall::time() {
-            let time = chrono::NaiveDateTime::from_timestamp_opt(t as i64, 0).unwrap();
+            let time = chrono::DateTime::from_timestamp(t as i64, 0).unwrap();
             println!(
                 "{}-{}-{} {}:{}:{}",
                 time.year(),

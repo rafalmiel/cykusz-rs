19d41090df6091b9d7843e69b7ead70cb0da3312
Rafal Miel <3881998+rafalmiel@users.noreply.github.com>
2025-01-02 18:42:56 +0000

    Don't hardcode dockerfile WORKSPACE path

--- sysroot/make_docker_image.sh
+++ sysroot/make_docker_image.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-SPATH=$(dirname $(readlink -f "$0"))
-CYKUSZ_DIR=$(realpath $SPATH/..)
+set -x
 
-docker buildx build -t cykusz-build --build-arg uid=$(id -u) --build-arg gid=$(id -g) $CYKUSZ_DIR/sysroot/cfg/docker
+SPATH=$(dirname "$(readlink -f "$0")")
+
+sed -i "s#{WORKDIR}#$SPATH#g" $SPATH/cfg/docker/Dockerfile
+
+docker buildx build -t cykusz-build --build-arg uid="$(id -u)" --build-arg gid="$(id -g)" "$SPATH"/cfg/docker

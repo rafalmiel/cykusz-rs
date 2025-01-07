5e1be162cb7959ad734c50b5bdbfd5ede4ef8855
Rafal Miel <3881998+rafalmiel@users.noreply.github.com>
2025-01-07 18:28:37 +0000

    fix toolchain docker build

--- sysroot/make_docker_image.sh
+++ sysroot/make_docker_image.sh
@@ -4,6 +4,4 @@ set -x
 
 SPATH=$(dirname "$(readlink -f "$0")")
 
-sed -i "s#{WORKDIR}#$SPATH#g" $SPATH/cfg/docker/Dockerfile
-
-docker buildx build -t cykusz-build --build-arg uid="$(id -u)" --build-arg gid="$(id -g)" "$SPATH"/cfg/docker
+docker buildx build -t cykusz-build --build-arg uid="$(id -u $(logname))" --build-arg gid="$(id -g $(logname))" "$SPATH"/cfg/docker

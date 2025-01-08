c08e984843a5bab554636410f3825189753ca11a
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2025-01-08 12:41:19 +0000

    Improve docker image

--- disk-scripts/make_disk.sh
+++ disk-scripts/make_disk.sh
@@ -11,8 +11,9 @@ parted -- disk.img mkpart primary ext2 4GiB 4.5Gib # 64
 parted disk.img set 1 boot on
 
 lo=$(losetup -f)
+u=$(logname)
 losetup -P $lo disk.img
-sudo -u $USER mkfs.ext2 "$lo"p1
-sudo -u $USER mkfs.ext2 "$lo"p2
-sudo -u $USER mkfs.ext2 "$lo"p3
+sudo -u $u mkfs.ext2 "$lo"p1
+sudo -u $u mkfs.ext2 "$lo"p2
+sudo -u $u mkfs.ext2 "$lo"p3
 losetup -d $lo
--- sysroot/make_docker_image.sh
+++ sysroot/make_docker_image.sh
@@ -3,5 +3,6 @@
 set -x
 
 SPATH=$(dirname "$(readlink -f "$0")")
+CYKUSZ_DIR=$(realpath $SPATH/..)
 
-docker buildx build -t cykusz-build --build-arg uid="$(id -u $(logname))" --build-arg gid="$(id -g $(logname))" "$SPATH"/cfg/docker
+docker buildx build -t cykusz-build --build-arg uid="$(id -u $(logname))" --build-arg gid="$(id -g $(logname))" --build-arg user=$(logname) --build-arg workdir="$CYKUSZ_DIR"  "$SPATH"/cfg/docker
